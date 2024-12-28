7d4b576cbc48364003fcb973691348eff874c207
Trương Huỳnh Anh Dũng <150664748+vodanh138@users.noreply.github.com>
2024-12-28 19:38:35 +0700

    28/12/20242024

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -55,6 +55,6 @@ class PostRepository extends BaseRepository implements PostRepositoryInterface
     }
     public function getPostsBelongTo($id)
     {
-        return $this->model->where('user_id', $id)->get();
+        return $this->model->where('user_id', $id)->orderBy('created_at', 'desc')->get();
     }
 }
